3434766178b36eef8c04aa0c736ead2617d6f906
0182lay <lay25king@gmail.com>
2026-06-02 23:40:33 +0700

    update app.ts server.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,6 +25,13 @@ app.get("/", (_req, res) => {
     });
 });
 
+app.get("/ping", (_req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "pong",
+    });
+});
+
 app.get("/api", (_req, res) => {
     return res.status(200).json({
         success: true,
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import { registerErrorHandlers } from "./middleware/error.middleware";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 const HOST = process.env.HOST || "0.0.0.0";
 
 const startServer = async () => {
